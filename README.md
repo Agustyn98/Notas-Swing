74bee2e403ac952c5409b6b0c76f14d15a1c8ff7
Agus <agustinadaro@hotmail.com>
2021-06-16 12:19:09 -0300

    mejora interfaz

---
+++ README.md
@@ -0,0 +1,66 @@
+# Trabajo de Programación Avanzada
+
+Aplicación de Notas
+Trabajo Integrador de Programación Avanzada - 2021
+
+Integrantes: Agustin Adaro – Santiago Vega
+
+Informe tambien como .docx
+
+
+
+La aplicación consiste en un almacenamiento de notas de texto plano, usando el sistema de gestión de base de datos SQLite.
+El desarrollo se hizo con el plugin Windows Builder de eclipse, que permite trabajar fácilmente con componentes de Swing, y con la librería jdbc, que nos permite conectarnos y hacer peticiones al archivo SQLite.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+**Funciones:**
+
+**1. Carpetas**
+
+Crear carpetas que agruparan diferentes notas que tengan alguna característica en común. Es la pantalla principal de la aplicación. Podemos crear, eliminar, editar y entrar a cada carpeta de la lista, además se pueden organizar según fecha de creación o modificación.
+
+ 
+![Carpetas4](https://user-images.githubusercontent.com/66125885/122243718-dd2b4380-ceb3-11eb-83ae-a6bae15bd6c4.png)
+
+
+
+**2. Lista notas**
+
+Las diferentes notas dentro de la carpeta seleccionada. Se toma como titulo la primera línea de nota completa. 
+En esta pantalla podemos crear una nueva nota, entrar a una nota existente, o eliminarla.
+Las notas también se pueden ordenar según fecha de creación o fecha de modificación.
+
+
+ 
+![notas123](https://user-images.githubusercontent.com/66125885/122243767-e6b4ab80-ceb3-11eb-9a96-d12a8489046a.png)
+
+
+
+
+**3. Nota:**
+
+Ver y modificar los contenidos cada nota. La primera línea de la nota se usará como título.
+En esta pantalla también podemos ver la fecha de la última modificación que habíamos realizado a esta.
+
+
+![vernota1283](https://user-images.githubusercontent.com/66125885/122243797-ec11f600-ceb3-11eb-88ec-3b62f67dd279.png)
+
+
+
+-----------------------------------------------------------------
+
+Para construir:
+Importar proyecto y libreria jdbc
+ 
---
+++ src/Database/ManejadorDB.java
@@ -0,0 +1,235 @@
+package Database;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import Modelos.Carpeta;
+import Modelos.Nota;
+
+public class ManejadorDB {
+    private String url = "jdbc:sqlite:notas.db";  
+
+    private static ManejadorDB instance = null;
+    public static ManejadorDB getInstance() {
+    	if(instance == null) {
+    		instance = new ManejadorDB();
+    	}
+    	return instance;
+    }
+    
+    public void crearTablas() {
+        String query = "CREATE TABLE IF NOT EXISTS folders(id INTEGER PRIMARY KEY, name TEXT)";  
+        String queryNota = "CREATE TABLE IF NOT EXISTS notes(id INTEGER PRIMARY KEY, text TEXT, dateModified TEXT, idFolder INTEGER)";
+        try{  
+            Connection conn = DriverManager.getConnection(url);  
+            Statement stmt = conn.createStatement();  
+            stmt.execute(query);  
+            stmt.execute(queryNota);
+            conn.close();
+            
+        } catch (Exception e) {  
+            System.out.println(e.getMessage());  
+        }
+    }
+    
+    public void agregarCarpeta(Carpeta carpeta) {
+    	String query = "INSERT INTO folders(name) VALUES (?)";
+        try{  
+            Connection conn = DriverManager.getConnection(url); 
+            PreparedStatement pstmt = conn.prepareStatement(query);  
+            pstmt.setString(1, carpeta.nombre);  
+            pstmt.executeUpdate(); 
+            conn.close();
+        } catch (Exception e) {  
+            System.out.println(e.getMessage());  
+        }  
+    }
+    public void editarCarpeta(int idCarpeta, String nombre) {
+    	String query = "UPDATE folders SET name='" + nombre + "'WHERE id=" + idCarpeta;
+    	
+    	try {
+            Connection conn = DriverManager.getConnection(url); 
+            PreparedStatement pstmt = conn.prepareStatement(query);  
+            pstmt.execute();
+            conn.close();
+            
+		}catch(Exception e) {
+            System.out.println(e.getMessage());  
+		}
+    }
+    public void agregarNota(Nota nota) {
+    	String query = "INSERT INTO notes(text,dateModified,idFolder) VALUES (?,?,?)";
+        try{  
+            Connection conn = DriverManager.getConnection(url); 
+            PreparedStatement pstmt = conn.prepareStatement(query);  
+            pstmt.setString(1, nota.texto);  
+            pstmt.setString(2, nota.fechaModificacion);  
+            pstmt.setInt(3, nota.idCarpeta);  
+            pstmt.executeUpdate(); 
+            conn.close();
+        } catch (Exception e) {  
+            System.out.println(e.getMessage());  
+        }  
+    }
+    public void editarNota(int idNota, String texto) {
+    	
+    	String query = "UPDATE notes SET text='" + texto + "'WHERE id=" + idNota;
+    	
+    	try {
+            Connection conn = DriverManager.getConnection(url); 
+            PreparedStatement pstmt = conn.prepareStatement(query);  
+            pstmt.execute();
+            conn.close();
+            
+		}catch(Exception e) {
+            System.out.println(e.getMessage());  
+		}
+    }
+	public ArrayList<Carpeta> seleccionarTodasCarpetas(int ordernarPor) {
+		String query = "SELECT * FROM folders";
+		switch(ordernarPor) {
+		case 1:
+			query = "SELECT * FROM folders ORDER BY id asc";
+			break;
+		case 2:
+			query = "SELECT * FROM folders ORDER BY id desc";
+			break;
+		case 3:
+			query = "SELECT * FROM folders ORDER BY name asc";
+			break;
+		}
+		var carpetas = new ArrayList<Carpeta>();
+		try {
+            Connection conn = DriverManager.getConnection(url); 
+            Statement stmt  = conn.createStatement();  
+            ResultSet rs    = stmt.executeQuery(query);  
+              
+            
+            while (rs.next()) {  
+            	/*
+                System.out.println(rs.getInt("id") +  "\t" +   
+                                   rs.getString("name"));*/
+                
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                carpetas.add(new Carpeta(id,name));
+            }
+            conn.close();
+		}catch (Exception e) {
+            System.out.println(e.getMessage());  
+		}
+		
+		return carpetas;
+	}
+	
+	public ArrayList<Nota> seleccionarNotas(int idCarpeta, int ordenarPor) {
+		String query = "SELECT * FROM notes";
+		switch(ordenarPor) {
+		case 1:
+			query = "SELECT * FROM notes WHERE idFolder = " + idCarpeta + " ORDER BY id asc";
+			break;
+		case 2:
+			query = "SELECT * FROM notes WHERE idFolder = " + idCarpeta + " ORDER BY id desc";
+			break;
+		case 3:
+			query = "SELECT * FROM notes WHERE idFolder = " + idCarpeta + " ORDER BY dateModified desc";
+			break;
+
+		}
+		
+
+		var notas = new ArrayList<Nota>();
+		try {
+            Connection conn = DriverManager.getConnection(url); 
+            Statement stmt  = conn.createStatement();  
+            ResultSet rs    = stmt.executeQuery(query);  
+              
+            
+            while (rs.next()) {  
+            	/*
+                System.out.println(rs.getInt("id") +  "\t" +   
+                                   rs.getString("name"));*/
+                
+                int id = rs.getInt("id");
+                String texto = rs.getString("text");
+                String fechaModificacion = rs.getString("dateModified");
+
+                notas.add(new Nota(id,texto,fechaModificacion,idCarpeta ));
+            }
+            conn.close();
+		}catch (Exception e) {
+            System.out.println(e.getMessage());  
+		}
+		
+		return notas;
+	}
+	
+	public void eliminarCarpeta(int id) {
+		String query = "DELETE FROM folders WHERE id = " + id;
+		try {
+            Connection conn = DriverManager.getConnection(url); 
+            PreparedStatement pstmt = conn.prepareStatement(query);  
+            pstmt.execute();
+            conn.close();
+            
+		}catch(Exception e) {
+            System.out.println(e.getMessage());  
+		}
+		eliminarNotasDeCarpeta(id);
+	}
+	
+	public void eliminarNotasDeCarpeta(int carpetaId) {
+		String query = "DELETE FROM notes WHERE idFolder = " + carpetaId;
+		try {
+            Connection conn = DriverManager.getConnection(url); 
+            PreparedStatement pstmt = conn.prepareStatement(query);  
+            pstmt.execute();
+            conn.close();
+            
+		}catch(Exception e) {
+            System.out.println(e.getMessage());  
+		}
+	}
+	
+	public void eliminarNota(int id) {
+		String query = "DELETE FROM notes WHERE id = " + id;
+		try {
+            Connection conn = DriverManager.getConnection(url); 
+            PreparedStatement pstmt = conn.prepareStatement(query);  
+            pstmt.execute();
+            conn.close();
+            
+		}catch(Exception e) {
+            System.out.println(e.getMessage());  
+		}
+	}
+	
+	public void actualizarNota(Nota nota) {
+		String query = "UPDATE notes SET text = '" + nota.texto + "', dateModified = '" + nota.fechaModificacion + "' WHERE id = " + nota.id;
+		try {
+            Connection conn = DriverManager.getConnection(url); 
+            PreparedStatement pstmt = conn.prepareStatement(query);  
+            pstmt.execute();
+            conn.close();
+		}catch(Exception e) {
+            System.out.println(e.getMessage());  
+		}
+	}
+	
+	public void actualizarUltimaNota(Nota nota) {
+		String query = "UPDATE notes SET text = '" + nota.texto + "', dateModified = '" + nota.fechaModificacion + "' WHERE id = (SELECT max(id) FROM notes)";
+		try {
+            Connection conn = DriverManager.getConnection(url); 
+            PreparedStatement pstmt = conn.prepareStatement(query);  
+            pstmt.execute();
+            conn.close();
+		}catch(Exception e) {
+            System.out.println(e.getMessage());  
+		}
+	}
+}
+
+
+
+
+
